ca403f7b2bc2848ee694e3e17f19d88e5d0de024
tiamo238-dotcom <tiamo238@gmail.com>
2026-08-10 14:51:29 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@
 // Publishable key(또는 anon public key)를 사용합니다.
 // service_role / secret key는 절대 넣지 마세요.
 
-const SUPABASE_URL = "https://ahmktrllwormidrcvthf.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ahmktrllwormidrcvthf.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_8gTnotOL-kRIcvCZk8HUDg_oaiF7naA";
